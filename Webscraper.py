0c89dacd255aa21faee5f455b50c8b9111267577
Daniel Owen <unlimiteddemi@gmail.com>
2023-06-10 02:23:09 +0100

    update

--- Webscraper.py
+++ Webscraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import base64
 
 def scrape_images(url, num_images):
     response = requests.get(url)
@@ -13,7 +14,10 @@ def scrape_images(url, num_images):
             image_urls.append(img["src"])
 
     for i, img_url in enumerate(image_urls[:num_images]):
-        save_image(img_url, i)
+        if img_url.startswith("data:image/"):
+            save_base64_image(img_url, i)
+        else:
+            save_image(img_url, i)
 
 def save_image(image_url, index):
     response = requests.get(image_url)
@@ -22,8 +26,15 @@ def save_image(image_url, index):
             file.write(response.content)
         print(f"Image {index}.jpg saved successfully.")
 
+def save_base64_image(base64_data, index):
+    _, data = base64_data.split(",", 1)
+    image_data = base64.b64decode(data)
+    with open(f"image_{index}.jpg", "wb") as file:
+        file.write(image_data)
+    print(f"Base64 image {index}.jpg saved successfully.")
+
 if __name__ == "__main__":
-    url = input("Enter the URL to scrap: ")
+    url = input("Enter the URL to scrape: ")
     num_images = int(input("Enter the number of images to scrape: "))
 
     scrape_images(url, num_images)
