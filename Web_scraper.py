2b2a16bd5492cbd70384c1c0e9a3b6d1e83fd6c8
Daniel Owen <unlimiteddemi@gmail.com>
2023-06-10 01:42:43 +0100

    first commit

--- Web_scraper.py
+++ Web_scraper.py
@@ -1,21 +1,29 @@
-import requests 
+import requests
 from bs4 import BeautifulSoup
+import os
 
-
-print("BBC HEADLINES THIS HOUR \n" *3 + "================\n" *2)
-
-def scrape_headlines():
-    url = "https://www.bbc.com/news"
+def scrape_images(url, num_images):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    headlines = soup.find_all("h3" )
-    for headline in headlines:
-        print(headline.text.strip())
+    img_tags = soup.find_all("img")
 
+    image_urls = []
+    for img in img_tags:
+        if img.has_attr("src"):
+            image_urls.append(img["src"])
 
+    for i, img_url in enumerate(image_urls[:num_images]):
+        save_image(img_url, i)
 
-        
+def save_image(image_url, index):
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        with open(f"image_{index}.jpg", "wb") as file:
+            file.write(response.content)
+        print(f"Image {index}.jpg saved successfully.")
+
 if __name__ == "__main__":
-    scrape_headlines()
-    
-   
+    url = input("Enter the URL to scrape: ")
+    num_images = int(input("Enter the number of images to scrape: "))
+
+    scrape_images(url, num_images)
